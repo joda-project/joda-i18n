273ac460483176d3e9b67ebc15390051e9dd4a8a
Tadej Novak <tadej@tano.si>
2017-07-09 15:41:03 +0200

    Add missing translation override function

---
+++ app/utils/i18n/missing-message.js
@@ -0,0 +1,28 @@
+// NOTE if ember-i18n add support for fallback locales we won't need this
+// https://github.com/jamesarosen/ember-i18n/issues/256
+
+import Ember from 'ember';
+import Locale from 'ember-i18n/utils/locale';
+
+const FALLBACK_LOCALE = 'en';
+
+let missingMessage = function(locale, key, data) {
+  if (locale === FALLBACK_LOCALE) {
+    return `Missing translation: ${key}`;
+  } else {
+    // NOTE This relies on internal APIs and is brittle.
+    // Emulating the internals of ember-i18n's translate method.
+    let i18n = this;
+
+    let count = Ember.get(data, 'count');
+
+    let defaults = Ember.makeArray(Ember.get(data, 'default'));
+    defaults.unshift(key);
+
+    let localeObj = new Locale(FALLBACK_LOCALE, Ember.getOwner(i18n));
+    let template = localeObj.getCompiledTemplate(defaults, count);
+    return template(data); // english fallback
+  }
+};
+
+export default missingMessage;
